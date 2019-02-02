abb7ed76d376a31bf07c0267d0dcf91e162ffaca
Thierry <tigraff@gmail.com>
2019-02-02 03:19:12 +0100

    Add command parsepage

---
+++ src/php/commands/parsepage.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+Parses a html page and outputs an array.
+
+@license    GPL
+@author    Thierry Graff
+@history    : 2019-02-02 02:47:43+01:00, Creation
+********************************************************************************/
+
+public class parsepage implements Command{
+    
+    public function execute($params){
+    }
+    
+}// end class
