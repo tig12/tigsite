355aef042d53004cdef27c07571efccdc386be19
Thierry <tigraff@gmail.com>
2019-02-02 02:51:46 +0100

    Bootstrap file run-tigsite.php ; first command : makenav

---
+++ src/php/commands/makenav.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+Generates navigation links for a part of a site.
+
+@license    GPL
+@author    Thierry Graff
+@history    : 2019-02-02 02:47:43+01:00, Creation
+********************************************************************************/
+
+public class makenav implements Command{
+    
+    public function execute(){
+    }
+    
+}// end class
